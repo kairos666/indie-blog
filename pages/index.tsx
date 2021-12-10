b0d727ac3b260c823bc8efa33f8da18384951500
David Maggi <musashi.miyamoto81@gmail.com>
2021-12-10 16:29:08 +0100

    Open Graph blank meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,10 @@ const Home: NextPage = () => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="manifest" href="site.webmanifest" />
                 <meta name="theme-color" content="#fafafa" />
+                <meta property="og:title" content=""/>
+                <meta property="og:type" content=""/>
+                <meta property="og:url" content=""/>
+                <meta property="og:image" content=""/>
           </Head>
 
           <main className={styles.main}>
