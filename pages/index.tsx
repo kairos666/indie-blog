74d5dbdfb279648dd2d73412d8c962747e85143d
David Maggi <musashi.miyamoto81@gmail.com>
2021-12-20 17:34:06 +0100

    successful static export with classical img tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -52,7 +51,7 @@ const Home: NextPage = () => {
             <footer className={styles.footer}>
                 <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">Powered by{' '}
                     <span className={styles.logo}>
-                        <Image src="/icon.svg" alt="Vercel Logo" width={72} height={16} />
+                        <img src="/icon.svg" width={ 16 } height={ 16 } alt="Vercel Logo" />
                     </span>
                 </a>
             </footer>
