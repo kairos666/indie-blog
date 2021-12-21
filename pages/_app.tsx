7fcee2f54509e3d709f0b0193f54e7d20e151a23
David Maggi <musashi.miyamoto81@gmail.com>
2021-12-21 23:08:51 +0100

    font choices - cleanup google font externals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.scss';
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
-import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 /* APP generic Error handling */
@@ -18,16 +17,6 @@ function ErrorFallback({ error }:FallbackProps) {
 function App({ Component, pageProps }: AppProps) {
     return (
         <ErrorBoundary FallbackComponent={ ErrorFallback }>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                {/* SC fonts */}
-                <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=WindSong:wght@400;500&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Petemoss&display=swap" rel="stylesheet" />
-                {/* D fonts */}
-            </Head>
             <Component {...pageProps} />
         </ErrorBoundary>
     );
