5856f4c4c7ea4ebee007440cb1eebe75c29ad8a3
David Maggi <musashi.miyamoto81@gmail.com>
2021-12-25 22:30:14 +0100

    PageIllustration component

---
+++ components/content/PageIllustration.tsx
@@ -0,0 +1,33 @@
+import { FC, Children, ReactElement, useState, useEffect } from 'react';
+import styles from '../../styles/cmpnts-partials/page-illustration.module.scss';
+
+type PageIllustrationProps = {};
+
+const PageIllustration:FC<PageIllustrationProps> = ({ children }) => {
+    const [imgNode, setImgNode] = useState(null);
+    const [figcaptionNode, setFigcaptionNode] = useState(null);
+
+    useEffect(() => {
+        const figCaption = Children.map(children, child => {
+                if((child as ReactElement).type === 'img') {
+                    setImgNode(child);
+                    return null;
+                } else {
+                    return child;
+                }
+            });
+        setFigcaptionNode(figCaption);
+    }, [children]);
+
+    return (
+        <figure className={ styles['page-illustration-container'] }>
+            { imgNode }
+            { figcaptionNode.length !== 0
+                ? <figcaption className={ styles['page-illustration-caption'] }>{ figcaptionNode }</figcaption>
+                : null
+            }
+        </figure>
+    )
+}
+
+export default PageIllustration;
