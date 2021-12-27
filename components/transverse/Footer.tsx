d83c72f622520a1791933138cd7839189badaf42
David Maggi <musashi.miyamoto81@gmail.com>
2021-12-27 16:46:14 +0100

    footer styles

--- components/transverse/Footer.tsx
+++ components/transverse/Footer.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
+import styles from '../../styles/cmpnts-partials/footer.module.scss';
+
 type FooterProps = {};
 
 const Footer:FC<FooterProps> = () => {
     return (
-        <footer>
-            <p>Capsule Corp. 2021</p>
+        <footer className={ styles['footer-container'] }>
+            <p className={ styles['footer-copyright'] }>Capsule Corp. 2021</p>
         </footer>
     )
 }
