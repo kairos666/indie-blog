ea1c882dce89cded3fbab8367fcafd88e6139579
David Maggi <musashi.miyamoto81@gmail.com>
2021-12-27 16:46:05 +0100

    header styles

--- components/transverse/Header.tsx
+++ components/transverse/Header.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
+import styles from '../../styles/cmpnts-partials/header.module.scss';
+
 type HeaderProps = {};
 
 const Header:FC<HeaderProps> = () => {
     return (
-        <header>
-            <h1>Kaïros</h1>
-            <p>Cabinet de curiosités numériques</p>
+        <header className={ styles['header-container'] }>
+            <div className={ styles['header-inner-container'] }>
+                <h1 className={ styles['header-logo'] }>Kaïros</h1>
+                <p className={ styles['header-motto'] }>Cabinet de curiosités numériques</p>
+            </div>
         </header>
     )
 }
